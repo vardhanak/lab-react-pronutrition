ef397aa5744f8b6113260f9f69bd8ff79b6a1ff4
vardhanak <achantakrishnavardhan@gmail.com>
2022-03-11 15:35:38 +0530

    first

---
+++ nutrition/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import FoodBox from './components/FoodBox';
+
+function App() {
+  return (
+   <>
+   <div className='header'>Pro-nutrition</div>
+<FoodBox />
+   </>
+  );
+}
+
+export default App;
